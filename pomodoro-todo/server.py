99b24efead0cd3cdb5566aa3b3b77b8dc9873e4d
Ismail Hashi <ismail.hashi101@gmail.com>
2019-01-06 17:36:17 -0500

    set init flask application.

--- pomodoro-todo/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-app.route('/')
-def index():
-    return '<h1>Hello!</h1>'
-
-if __name__ == "__main__":
-    app.run(port=4996, debug=True)
---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import logging
+
+articleURL = "https://www.washingtonpost.com/news/energy-environment/wp/2017/02/22/oklahoma-attorney-generals-office-releases-7500-pages-of-emails-between-scott-pruitt-and-fossil-fuel-industry/"
+#text = getTextWaPo(articleURL)
+
+#logger = logging.basicConfig(filename='logfile.log', level=logging.INFO)
+app = Flask(__name__)
+
+
+@app.route('/')
+def send_form():
+    return render_template('main.html')
+
+
+#@app.route('/abstres', methods=['POST'])
+#def send_data():
+    #3artUrl = request.form['artUrl']
+    #text = getTextWaPo(artUrl)
+    #if not text:
+        #return "that wasn't an acceptable url buddy"
+    #return summarize(text)
+
+
+def run_server():
+    app.debug = True
+
+
+if __name__ == "__main__":
+    run_server()
