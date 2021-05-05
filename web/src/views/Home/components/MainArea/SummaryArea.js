4ffeba9867c3caf6662a5d4b980efe4673fb5825
Ismail Hashi <ismail.hashi101@gmail.com>
2021-05-05 15:22:11 -0400

    added circular progressbar when summarizing

--- web/src/views/Home/components/MainArea/SummaryArea.js
+++ web/src/views/Home/components/MainArea/SummaryArea.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+import { green } from "@material-ui/core/colors";
 
 // import PublishIcon from "@material-ui/icons/Publish";
 // import IconButton from "@material-ui/core/IconButton";
@@ -83,7 +86,7 @@ const useStyles = makeStyles((theme) => ({
     letterSpacing: "0.015rem",
     display: "flex",
     width: "100%",
-    alignItems: "center",
+    alignItems: "center"
   },
   summaryResults: {
     paddingTop: "40px",
@@ -112,16 +115,25 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3, 0, 0),
     margin: "0 auto",
   },
+  buttonProgress: {
+    color: green[500],
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
+  }
 }));
 
 export default function SummaryArea() {
   const classes = useStyles();
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
   const [url, setUrl] = React.useState("");
   //const [selectedFile, setSelectedFile] = useState();
   //const [isFilePicked, setIsFilePicked] = useState(false);
   const [summaryBox, setSummaryBox] = React.useState("");
   const [sentences, setSentences] = React.useState("7");
-
   const [summaryData, setSummaryData] = useState([
     {
       url: url,
@@ -186,7 +198,16 @@ export default function SummaryArea() {
   //   //console.log(event.target.files[0]);
   // };
 
+  const handleButtonClick = () => {
+    if (!loading) {
+      setSuccess(false);
+      setLoading(true);
+    }
+  };
+
   const summarizeArticle = () => {
+    handleButtonClick()
+
     // const formData = new FormData();
 
     // formData.append("File", selectedFile);
@@ -207,6 +228,8 @@ export default function SummaryArea() {
       })
       .then((response) => {
         console.log("We good", response.data);
+        setSuccess(true);
+        setLoading(false);
       })
       .catch(function (error) {
         if (error.response) {
@@ -309,16 +332,19 @@ export default function SummaryArea() {
             />
           </form>
         </div>
-        <CircularProgress color="secondary" />
         <div className={classes.buttonContainer}>
           <Button
+            className={classes.summarizeButton}
             onClick={summarizeArticle}
             variant="contained"
             size="large"
             color="secondary"
-            className={classes.summarizeButton}
+            disabled={loading}
           >
             Summarize
+            {loading && (
+            <CircularProgress size={24} className={classes.buttonProgress} />
+          )}
           </Button>
         </div>
       </div>
