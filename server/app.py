5413b797f87cfa8a9571761f00015fccb1609255
Ismail Hashi <ismail.hashi101@gmail.com>
2021-06-14 14:27:40 -0400

    testing heroku

--- server/app.py
+++ server/app.py
@@ -1,51 +1,54 @@
-import time
-from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+# import time
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS, cross_origin
 
-from server import summarize
+# from server import summarize
+
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 
-@app.route("/summarize", methods=["POST"])
-def summarizeText():
-    start = time.time()
+# @app.route("/summarize", methods=["POST"])
+# def summarizeText():
+#     start = time.time()
 
-    requestData = request.get_json()
-    data = requestData["summary"]
+#     requestData = request.get_json()
+#     data = requestData["summary"]
 
-    articleURL = data["url"]
-    textBody = data["summaryBox"]
-    sentences = data["sentences"]
+#     articleURL = data["url"]
+#     textBody = data["summaryBox"]
+#     sentences = data["sentences"]
 
-    if articleURL:
-        title, strippedText = summarize.getText(articleURL)
+#     if articleURL:
+#         title, strippedText = summarize.getText(articleURL)
 
-    if textBody:
-        summaryText = textBody
-    else:
-        summaryText = strippedText
+#     if textBody:
+#         summaryText = textBody
+#     else:
+#         summaryText = strippedText
 
-    shortUrl = summarize.getDomainName(articleURL)
-    keywords = summarize.getKeywords(summaryText)
-    summarizedSentences, summarizedText = summarize.summarizeText(
-        articleURL, int(sentences)
-    )
-    end = time.time()
-    summaryTime = summarize.getSummaryTime(start, end)
-    contentReducedPercentage = summarize.contentReducedBy(summaryText, summarizedText)
-    summary = summarize.getSummary(
-        shortUrl,
-        title,
-        summarizedSentences,
-        summaryTime,
-        contentReducedPercentage,
-        sentences,
-        keywords,
-    )
+#     shortUrl = summarize.getDomainName(articleURL)
+#     keywords = summarize.getKeywords(summaryText)
+#     summarizedSentences, summarizedText = summarize.summarizeText(
+#         articleURL, int(sentences)
+#     )
+#     end = time.time()
+#     summaryTime = summarize.getSummaryTime(start, end)
+#     contentReducedPercentage = summarize.contentReducedBy(summaryText, summarizedText)
+#     summary = summarize.getSummary(
+#         shortUrl,
+#         title,
+#         summarizedSentences,
+#         summaryTime,
+#         contentReducedPercentage,
+#         sentences,
+#         keywords,
+#     )
 
-    return jsonify(summary)
+#     return jsonify(summary)
 
 @app.route("/hello")
 def hello():
